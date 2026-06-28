ed22da85d276d7700b6cd451cf623382b0a48b31
elx139013901390-tech <elx139013901390@gmail.com>
2026-06-29 02:08:23 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+OWNER_NAME = "امیر علی فروزان اصل"
+
+START_TEXT = f"""
+🤖 ربات مدیریت گروه
+
+👨‍💻 سازنده: {OWNER_NAME}
+
+✅ ربات با موفقیت راه‌اندازی شد.
+"""
